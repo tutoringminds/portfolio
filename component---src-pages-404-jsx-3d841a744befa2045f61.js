3cefc56f205a24ea7a9a964cb88c65c0d291a84c
shiveshrajnigam <33284557+shiveshrajnigam@users.noreply.github.com>
2021-02-04 01:41:17 +0530

    Updates

---
+++ component---src-pages-404-jsx-3d841a744befa2045f61.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{pssB:function(e,n,t){"use strict";t.r(n);var l=t("q1tI"),a=t("iepH");n.default=function(){return l.createElement(a.a,null,l.createElement("div",{style:{textAlign:"center"}},l.createElement("h1",null,"404"),l.createElement("p",null,"Page not found.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-3d841a744befa2045f61.js.map
